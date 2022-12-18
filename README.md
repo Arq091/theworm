3b2a0587926b76a7ff3b99182da0f4283d788ae9
BrokenFridge <103411852+Arq091@users.noreply.github.com>
2022-12-18 08:42:07 +0700

    added menu feature

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# **THE WORM**
+# **THE WORM V2**
 
-## Updates
+# Changelog
+
+## v2.1
+
+- Menu is now minimizable
+- Mobile verison works better
+- Slightly modified UI
+- Fixed _some_ bugs (there are still **many** more, trust me)
+
+## v2.0
 
 - Redesigned UI
-- Responsive (?)
 - Now with "Worm Length"
-- Coming **very** soon...
--- pages/index.jsx
+++ pages/index.jsx
@@ -18,19 +18,86 @@ export default function Home() {
     length: 6,
     color: 200,
   })
+  const [isOpen, setIsOpen] = useState(true)
 
   return (
-    <div className="app">
+    <div
+      className="app"
+      style={{
+        backgroundColor: `hsl(${wormShit.color}, 85%, 90%)`,
+        paddingLeft: `${isOpen ? "29rem" : "0"}`,
+      }}
+    >
       <Head>
-        <title>The Worm - Wiggly Worm V2</title>
+        <title>Wiggly Worm - The Worm</title>
         <meta name="description" content="Just a worm." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav className="top-items">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler "
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          onClick={() => setIsOpen(true)}
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <line x1="4" y1="6" x2="20" y2="6"></line>
+          <line x1="4" y1="12" x2="20" y2="12"></line>
+          <line x1="4" y1="18" x2="20" y2="18"></line>
+        </svg>
+        <a href="https://github.com/arq091/theworm" target="blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler github"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            strokeWidth="1.6"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+          </svg>
+        </a>
+      </nav>
 
-      <section className="wrapper border">
-        <h1 className="text-bold">
-          Wiggly Worm<sup>v2</sup>
-        </h1>
+      <section
+        className="sidebar border"
+        style={{ transform: `translateX(${isOpen ? "0%" : "-100%"})` }}
+      >
+        <header className="text-bold header">
+          <h1>
+            Wiggly Worm<sup>v2</sup>
+          </h1>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-x"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            onClick={() => setIsOpen(false)}
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </header>
         <div>
           <h2 className="text-underline text-normal">
             What is this monstrosity?
@@ -39,7 +106,7 @@ export default function Home() {
             This <strong>thing </strong>
             is basically the second version of a stupid project I made like a
             year ago for fun. Yes, this is literally it. Btw, this thing has
-            like 10 bugs lol sorry. Uhh... Have fun, I guess?
+            like 1000 bugs lol sorry.
           </p>
         </div>
         <div>
@@ -112,10 +179,7 @@ export default function Home() {
           </Formik>
         </div>
       </section>
-      <main
-        className="board"
-        style={{ backgroundColor: `hsl(${wormShit.color}, 90%, 93%)` }}
-      >
+      <main className="board">
         <div className="worm">
           {[...Array(wormShit.length)].map((i, index) => {
             if (index != 0) {
@@ -128,9 +192,10 @@ export default function Home() {
                     width: `${wormShit.size}px`,
                     height: `${wormShit.size}px`,
                     backgroundColor: `hsl(${wormShit.color}, 50%, ${
-                      index * 2.7 + 40
+                      index * 2.6 + 35
                     }%)`,
                     borderRadius: `${wormShit.size / 7}px`,
+                    left: `-${index * 0.25}rem`,
                   }}
                 ></div>
               )
@@ -144,7 +209,7 @@ export default function Home() {
                     width: `${wormShit.size}px`,
                     height: `${wormShit.size}px`,
                     backgroundColor: `hsl(${wormShit.color}, 50%, ${
-                      index * 3 + 40
+                      index * 3 + 35
                     }%)`,
                     borderRadius: `${wormShit.size / 7}px`,
                     padding: `${wormShit.size / 10}px`,
